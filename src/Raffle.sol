5812f988050735c47abfbf1644c587218a2f1bff
Marco Bollini <mabollini@fi.uba.ar>
2026-02-14 16:10:26 -0300

    .

---
+++ src/Raffle.sol
@@ -0,0 +1,66 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title Ruffle contract
+ * @author Marco Bollini
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Ruffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRuffle();
+
+    uint256 private immutable i_entranceFee;
+    // @dev the duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRuffle() external payable {
+        if (msg.value < i_entranceFee) revert Raffle__SendMoreToEnterRuffle();
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+    }
+
+    // --- getters ---
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
